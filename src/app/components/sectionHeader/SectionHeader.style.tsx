ffa72e41b62a7f8a72dc22634f883d7c29218390
praveenbalajis <achillies.balaji@gmail.com>
2023-05-06 16:27:45 +0530

    header css upadate

--- src/app/components/sectionHeader/SectionHeader.style.tsx
+++ src/app/components/sectionHeader/SectionHeader.style.tsx
@@ -4,7 +4,8 @@ import { StSectionFont } from "../../../Index.style";
 export const StSectionHeaderContainer = styled.div`
     box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px 10px,
         rgb(51, 51, 51, 0.5) 0px 0px 0px 3px;
-    max-width: 55%;
+    min-width: 55%;
+    max-width: fit-content;
     margin: 0 auto;
     padding: 15px;
 `;
