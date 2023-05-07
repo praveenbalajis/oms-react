244f940b8d9c627567920bfd13cb75a9eec469d6
praveenbalajis <achillies.balaji@gmail.com>
2023-05-07 19:08:52 +0530

    Home Css

--- src/app/modules/home/Home.style.tsx
+++ src/app/modules/home/Home.style.tsx
@@ -1,27 +1,5 @@
 import styled from "styled-components";
 
 export const StHomeSection = styled.div`
-    padding: 2rem;
-    margin-top: 2rem;
-`;
-
-export const StCardContainer = styled.div`
-    margin-top: 3rem;
-    padding: 2rem;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-`;
-
-export const StCardItem = styled.div`
-    width: 26%;
-    min-height: 250px;
-    background-color: white;
-    border: 1px solid #ebebeb;
-    box-shadow: rgba(111, 46, 240, 0.4) -5px 5px,
-        rgba(111, 46, 240, 0.3) -10px 10px, rgba(111, 46, 240, 0.2) -15px 15px,
-        rgba(111, 46, 240, 0.1) -20px 20px, rgba(111, 46, 240, 0.05) -25px 25px;
-    padding: 2rem;
-    margin-bottom: 5rem;
+    margin-bottom: 4rem;
 `;
--- src/app/modules/home/Home.tsx
+++ src/app/modules/home/Home.tsx
@@ -1,37 +1,15 @@
 import Jumbotron from "../../components/jumbotron/Jumbotron";
-import SectionContainer from "../../components/sectionContainer/SectionContainer";
-import SectionHeader from "../../components/sectionHeader/SectionHeader";
 import AboutMe from "../aboutMe/AboutMe";
 import Skills from "../skills/Skills";
-// import { StHomeSection } from "./Home.style";
+import { StHomeSection } from "./Home.style";
 
 const Home = (props: any) => {
     return (
-        <>
+        <StHomeSection>
             <Jumbotron />
             <AboutMe />
             <Skills />
-            <SectionContainer>
-                <SectionHeader header={"Welcome to the Playground"} subHeader={"Select one of the apps to play with"} />
-                {/* <StCardContainer>
-                    <StCardItem>
-                        To do lists
-                    </StCardItem>
-                    <StCardItem>
-                        To do lists
-                    </StCardItem>
-                    <StCardItem>
-                        To do lists
-                    </StCardItem>
-                    <StCardItem>
-                        To do lists
-                    </StCardItem>
-                    <StCardItem>
-                        Messenger - kafka
-                    </StCardItem>
-                </StCardContainer> */}
-            </SectionContainer>
-        </>
+        </StHomeSection>
     );
 };
 
